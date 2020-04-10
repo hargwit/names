972c54a5a99eccf09cc9cf500ac4bda91d7fd0f6
Harry G Withers <harrygwithers@gmail.com>
2020-04-10 23:10:19 +0100

    end turn api

---
+++ src/play/api.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import db from '../firebase'
+
+const gameRef = (id) => db.collection('games').doc(id)
+
+const useEndTurn = (gameCode, round) => {
+  const [loading, setLoading] = useState(false)
+
+  const endTurn = (names) => {
+    setLoading(true)
+    return db.runTransaction((transaction) => {
+      return transaction.get(gameRef(gameCode)).then((result) => {
+        if (!result.exists) {
+          throw new Error('No game by this code')
+        }
+
+        const game = result.data()
+
+        const newNames = game.names.map((name) => {
+          if (names.includes(name.value)) {
+            return {
+              ...name,
+              lastRound: round,
+            }
+          }
+          return name
+        })
+
+        transaction.update(gameRef(gameCode), {
+          names: newNames,
+          currentPlayer: '',
+        })
+      })
+    })
+  }
+
+  return endTurn
+}
+
+export { useEndTurn }
