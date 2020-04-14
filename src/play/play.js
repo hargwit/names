f44367e09f49d7c3c138d7d1260ee6af90d96f90
Harry G Withers <harrygwithers@gmail.com>
2020-04-14 08:02:24 +0100

    simple modal

--- src/play/play.js
+++ src/play/play.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGame } from '../game/game-provider'
 import { Container } from '../layout/container'
-import { Button } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import { usePlay } from './play-reducer'
 import { useEndTurn } from './api'
 import { Loading } from '../loading/loading'
@@ -52,6 +52,7 @@ const Play = ({ round }) => {
     passName,
     unPassName,
   } = usePlay(gameNames)
+  const [showModal, setShowModal] = useState(false)
 
   useEffect(() => {
     setNewName()
@@ -70,7 +71,16 @@ const Play = ({ round }) => {
   }
 
   function handleEndTurn() {
+    handleCloseModal()
     endTurn(completedNames)
+  }
+
+  function handleCloseModal() {
+    setShowModal(false)
+  }
+
+  function handleOpenModal() {
+    setShowModal(true)
   }
 
   const namesLeft = gameNames.length - completedNames.length
@@ -78,53 +88,86 @@ const Play = ({ round }) => {
   return loading ? (
     <Loading />
   ) : (
-    <Container>
-      <h1 style={styles.name}>{currentName ? currentName : 'No more names'}</h1>
-      <div style={styles.playInfo}>
-        {nextName ? (
+    <>
+      <Container>
+        <h1 style={styles.name}>
+          {currentName ? currentName : 'No more names'}
+        </h1>
+        <div style={styles.playInfo}>
+          {nextName ? (
+            <p>
+              Next name: <strong>{nextName}</strong>
+            </p>
+          ) : (
+            <p>
+              Current pass: <strong>{currentPass || '-'}</strong>
+            </p>
+          )}
           <p>
-            Next name: <strong>{nextName}</strong>
+            Score: <strong>{completedNames.length}</strong>
           </p>
-        ) : (
-          <p>
-            Current pass: <strong>{currentPass || '-'}</strong>
-          </p>
-        )}
-        <p>
-          Score: <strong>{completedNames.length}</strong>
-        </p>
-      </div>
-      <div style={styles.buttons}>
-        <div style={styles.playControls}>
+        </div>
+        <div style={styles.buttons}>
+          <div style={styles.playControls}>
+            <Button
+              disabled={!currentName}
+              onClick={handleNext}
+              style={styles.playButton}
+              variant='primary'
+              size='lg'
+            >
+              Next!
+            </Button>
+            <Button
+              disabled={!currentPass && namesLeft < 2}
+              onClick={handlePass}
+              style={styles.playButton}
+              variant='warning'
+              size='lg'
+            >
+              {currentPass ? 'Unpass!' : 'Pass!'}
+            </Button>
+          </div>
           <Button
-            disabled={!currentName}
-            onClick={handleNext}
-            style={styles.playButton}
+            onClick={handleOpenModal}
+            style={styles.button}
             variant='primary'
             size='lg'
           >
-            Next!
-          </Button>
-          <Button
-            disabled={!currentPass && namesLeft < 2}
-            onClick={handlePass}
-            style={styles.playButton}
-            variant='warning'
-            size='lg'
-          >
-            {currentPass ? 'Unpass!' : 'Pass!'}
+            End turn
           </Button>
         </div>
-        <Button
-          onClick={handleEndTurn}
-          style={styles.button}
-          variant='primary'
-          size='lg'
-        >
-          End turn
-        </Button>
-      </div>
-    </Container>
+      </Container>
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header>
+          <Modal.Title>Confirm your score</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          You scored <strong>{completedNames.length}</strong> name
+          {completedNames.length !== 1 && 's'}!
+          {completedNames.length > 0 && (
+            <div>
+              Your names:
+              <ul>
+                {completedNames.map((name, index) => (
+                  <li key={name + index}>
+                    <strong>{name}</strong>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleCloseModal}>
+            Cancel
+          </Button>
+          <Button variant='primary' onClick={handleEndTurn}>
+            Confirm and end turn
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 
