5b7bc6b61ba8d279631cd81a7eb47dbb764ef130
Harry G Withers <harrygwithers@gmail.com>
2020-04-21 08:25:07 +0100

    remove and put back buttons

--- src/play/end-turn.js
+++ src/play/end-turn.js
@@ -1,25 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Modal } from 'react-bootstrap'
 
+const styles = {
+  name: (removed) => ({
+    textDecoration: removed ? 'line-through' : 'none',
+  }),
+}
+
 const EndTurnModal = ({ show, handleClose, handleEndTurn, names }) => {
+  const [confirmedNames, setConfirmedNames] = useState([])
+
+  useEffect(() => {
+    names.map((name) => ({ ...name, removed: false }))
+    setConfirmedNames(names)
+  }, [names])
+
+  function removeName(id) {
+    const newNames = confirmedNames.map((n) =>
+      n.id === id
+        ? {
+            ...n,
+            removed: true,
+          }
+        : n,
+    )
+    setConfirmedNames(newNames)
+  }
+
+  function putBack(id) {
+    const newNames = confirmedNames.map((n) =>
+      n.id === id
+        ? {
+            ...n,
+            removed: false,
+          }
+        : n,
+    )
+    setConfirmedNames(newNames)
+  }
+
   return (
     <Modal show={show} onHide={handleClose} centered>
       <Modal.Header>
         <Modal.Title>Confirm your score</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        You scored <strong>{names.length}</strong> name
-        {names.length !== 1 && 's'}!
-        {names.length > 0 && (
+        You scored <strong>{confirmedNames.filter(notRemoved).length}</strong>{' '}
+        name
+        {confirmedNames.filter(notRemoved).length !== 1 && 's'}!
+        {confirmedNames.length > 0 && (
           <div>
             Your names:
-            <ul>
-              {names.map((name) => (
-                <li key={name.id}>
-                  <strong>{name.value}</strong>
-                </li>
+            <table>
+              {confirmedNames.map((name) => (
+                <tr key={name.id}>
+                  <td>
+                    <strong style={styles.name(name.removed)}>
+                      {name.value}
+                    </strong>
+                  </td>
+                  <td>
+                    {name.removed ? (
+                      <Button onClick={() => putBack(name.id)}>Put back</Button>
+                    ) : (
+                      <Button onClick={() => removeName(name.id)}>
+                        Remove
+                      </Button>
+                    )}
+                  </td>
+                </tr>
               ))}
-            </ul>
+            </table>
           </div>
         )}
       </Modal.Body>
@@ -29,13 +80,19 @@ const EndTurnModal = ({ show, handleClose, handleEndTurn, names }) => {
         </Button>
         <Button
           variant='primary'
-          onClick={() => handleEndTurn(names.map((n) => n.id))}
+          onClick={() =>
+            handleEndTurn(confirmedNames.filter(notRemoved).map((n) => n.id))
+          }
         >
           Confirm and end turn
         </Button>
       </Modal.Footer>
     </Modal>
   )
+}
+
+function notRemoved(name) {
+  return !name.removed
 }
 
 export { EndTurnModal }
