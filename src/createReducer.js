fbd1929fdbf7a78e0025d08fb1c7078bc84568a2
Harry G. Withers <harrygwithers@gmail.com>
2020-08-21 07:23:49 +0100

    install and use vanilla-create-reducer

--- src/createReducer.js
+++
@@ -1,9 +0,0 @@
-export default function createReducer(actionMapping) {
-  return function (state, action) {
-    if (actionMapping[action.type] === undefined) {
-      throw new Error(`Unknown action type ${action.type}`)
-    }
-
-    return actionMapping[action.type](state, action)
-  }
-}
